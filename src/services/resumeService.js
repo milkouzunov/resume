6c7a2e624a1a08198761a412e308b82b0ec485b2
Milko Uzunov <72871764+milkouzunov@users.noreply.github.com>
2021-08-11 22:03:47 +0300

    Update resumeService.js

--- src/services/resumeService.js
+++ src/services/resumeService.js
@@ -1,9 +1,9 @@
-const url = process.env.REACT_APP_API_URL;
+const url = 'https://rest-api-resume.herokuapp.com/api/main';
 
 async function getResumeData() {
    
     try {
-        const data = await fetch(`${url}/main`);
+        const data = await fetch(url);
         return data.json();
     } catch (err) {
         console.error(err);
